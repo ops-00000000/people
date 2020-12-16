1b26a922094336bc4e903eb0511fd2680fa88844
Vladimir <belaevvova999@gmail.com>
2020-12-16 21:00:01 +0300

    Second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,12 @@
 fun main(){
-    val likes = 32
-    if (likes % 2 == 0){
+    val likes = 11
+    if (likes == 11){
+        println("Понравилось $likes людям")
+    }
+    else if (likes % 100 == 11){
+        println("Понравилось $likes людям")
+    }
+    else if (likes % 2 == 0) {
         println("Понравилось $likes людям")
     }
     else{
